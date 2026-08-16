905802f0a6c0eb028efe9b24271bcdda483294cf
Kenu4000 <komabuchi4000@gmail.com>
2026-08-16 09:00:46 +0900

    無設定の直前再確認をService Workerへ追加

--- browser-extension/service-worker.js
+++ browser-extension/service-worker.js
@@ -1,1 +1,5 @@
-importScripts("snapshot-readiness-wrapper.js", "fast-site-mode-wrapper.js");
+importScripts(
+  "snapshot-readiness-wrapper.js",
+  "fast-site-mode-wrapper.js",
+  "crawl-disabled-preflight-wrapper.js",
+);
