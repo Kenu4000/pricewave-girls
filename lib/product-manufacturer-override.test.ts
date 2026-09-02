6de824f07d800087d92b33adc1c6b4b2181607c3
Kenu4000 <komabuchi4000@gmail.com>
2026-09-02 12:49:37 +0900

    商品単位メーカー補正を回帰テストする

---
+++ lib/product-manufacturer-override.test.ts
@@ -0,0 +1,19 @@
+import assert from "node:assert/strict";
+import test from "node:test";
+import { manufacturerForProduct } from "./product-manufacturer-override";
+
+test("メタルオレンジ EXカスタムはカスタムへ補正する", () => {
+  assert.equal(manufacturerForProduct("メタルオレンジ EXカスタム", "別メーカー"), "カスタム");
+  assert.equal(
+    manufacturerForProduct("メタルオレンジ EXカスタム（説明書欠け）", "別メーカー"),
+    "カスタム",
+  );
+});
+
+test("狂った果実はフェアリーテイルへ補正する", () => {
+  assert.equal(manufacturerForProduct("狂った果実", "別メーカー"), "フェアリーテイル");
+});
+
+test("他商品は元メーカーを保持する", () => {
+  assert.equal(manufacturerForProduct("別の商品", "Leaf"), "Leaf");
+});
